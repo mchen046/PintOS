3e82a88ff48bc68a1e6f78693c0b40747d2d16c3
ykamo001 <ykamo001@ucr.edu>
2016-01-29 16:43:18 -0800

    micheal genius

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -334,10 +334,9 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
 
   if (!list_empty (&cond->waiters))
   { 
-    sema_up (&list_entry (list_pop_front (&cond->waiters), struct semaphore_elem, elem)->semaphore);
+    sema_up (&list_entry (list_max (&cond->waiters, left_less_than_right, NULL), struct semaphore_elem, elem)->semaphore);
   }
 
-  yield_all_except_one();
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
